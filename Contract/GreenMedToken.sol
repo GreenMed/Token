2fa4ba04e52b0b15f6633ef8e32408951fe8786b
davedevrepo <daviddahan@gmail.com>
2017-10-25 12:23:54 -0700

    Added enforce freeze method

--- Contract/GreenMedToken.sol
+++ Contract/GreenMedToken.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+
 contract owned {
     address public owner;
     address public candidate;
@@ -25,6 +26,7 @@ contract owned {
     }
 }
 
+
 library SafeMath {
     function sub(uint256 a, uint256 b) pure internal returns (uint256) {
         assert(a >= b);
@@ -38,20 +40,21 @@ library SafeMath {
     }
 }
 
+
 contract ERC20 {
     uint256 public totalSupply;
-    function balanceOf( address who ) public constant returns (uint256 value);
-    function allowance( address owner, address spender ) public constant returns (uint256 _allowance);
+    function balanceOf(address who) public constant returns (uint256 value);
+    function allowance(address owner, address spender) public constant returns (uint256 _allowance);
+    function transfer(address to, uint256 value) public returns (bool success);
+    function transferFrom(address from, address to, uint256 value) public returns (bool success);
+    function approve(address spender, uint256 value) public returns (bool success);
 
-    function transfer( address to, uint256 value) public returns (bool success);
-    function transferFrom( address from, address to, uint256 value) public returns (bool success);
-    function approve( address spender, uint256 value ) public returns (bool success);
-
-    event Transfer( address indexed from, address indexed to, uint256 value);
-    event Approval( address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract GreenMed  is ERC20, owned {
+
+contract GreenMed is ERC20, owned {
     using SafeMath for uint256;
     string public name = "GreenMed";
     string public symbol = "GRMD";
@@ -79,31 +82,31 @@ contract GreenMed  is ERC20, owned {
         Transfer(0, msg.sender, totalSupply);
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(!frozenAccount[_from] && !frozenAccount[_to]);
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(_from, _to, _value);
-    }
-
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
+        require(_to != address(0));
+        require(balances[msg.sender] >= _value);
+        require(!frozenAccount[msg.sender] && !frozenAccount[_to]);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0));
         require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] = balances[_to].add(_value);
-        balances[_from] = balances[_from].sub(_value);
+        require(!frozenAccount[_from] && !frozenAccount[_to]);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         Transfer(_from, _to, _value);
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0));
         require(balances[msg.sender] >= _value);
+        require(!frozenAccount[_spender] && !frozenAccount[msg.sender]);
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
